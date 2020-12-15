19b2221bc65f2eba0385a51cbabb1cf336690071
mickmccann <mickmccann@ymail.com>
2020-12-15 16:22:48 +0000

    Getting used to the canvas element by drawing simple boxes

--- assets/script/asteroids.js
+++ assets/script/asteroids.js
@@ -1,2 +1,14 @@
 let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//c variable stands for context
+let c = canvas.getContext('2d');
+
+c.fillRect(100, 100, 100, 100)
+c.fillRect(10, 10, 10, 100)
+c.fillRect(100, 300, 100, 100)
+c.fillRect(800, 100, 100, 500)
+c.fillRect(100, 900, 400, 100)
+
 console.log(canvas);
