13e43802e1d0261143fcca63c27b2816fc9b450f
mickmccann <mickmccann@ymail.com>
2020-12-17 15:33:36 +0000

    Added a background image to the canvas. In my javascript file I putin the basic foundations of what I want to include

--- assets/script/asteroids.js
+++ assets/script/asteroids.js
@@ -1,45 +1,26 @@
-let canvas;
-let ctx;
-let canvasWidth = 1400;
-let canvasHeight = 1000;
-let keys = [];
+//code for Asteroids game
 
-document.addEventListener('DOMContentLoaded', SetupCanvas);
+const FPS = 30; //game's frames per second
 
-function SetupCanvas() {
-    canvas = document.getElementById('asteroid-canvas');
-    ctx = canvas.getContext('2d');
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-    ctx.fillStyle = 'black';
-    ctx.fillRect = (0, 0, canvas.width, canvas.height);
-    document.body.addEventListener('keydown', function(e){
-        keys[e.keyCode] = true; 
-    });
-    document.body.addEventListener('keyup', function(e){
-        keys[e.keyCode] = false; 
-    });
-    Render();
-}
-//Properties for the spaceship
-class Spaceship {
-    constructor() {
-        //Sometimes I want the spaceship to be visible and sometimes I do not. E.g. at the start of the game 
-        this.visible = true;
-        //Makes spaceship appear in centre of screen
-        this.x = canvasWidth / 2;
-        this.y = canvasHeight / 2;
-        this.movingForward = false;
-        this.speed = 0.1;
-        this.velocityX = 0;
-        this.velocityY = 0;
-        this.rotateSpeed = 0.001
-    }
+let canvas = document.getElementById('asteroid-canvas');
+let ctx = canvas.getContext('2d');
+
+//game loop to achieve the required animation
+setInterval(update, 1000 / FPS);
+
+function update() {
+    //to do list to build game:
+    //1. build a galaxy background
+    let background = new Image();
+    background.src = 'assets/images/nathan_anderson_unsplash_galaxy_image.jpg';
+
+    background.onload = function(){
+    ctx.drawImage(background,0, 0, canvas.width, canvas.height);   
 }
 
-//Creates a new ship object
-let spaceship = new Spaceship();
+    //2. build the spaceship
 
-function Render() {
+    //3. build functions to move the ship
 
-};
+    //4. build functions to rotate the ship
+}
