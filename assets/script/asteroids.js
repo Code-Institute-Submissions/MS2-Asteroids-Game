fbcc762c56589f6b414d84365dca3a2ce5950273
mickmccann <mickmccann@ymail.com>
2020-12-17 18:04:49 +0000

    In the javascript file, I made a part of the triangle that'll represent the spaceship

--- assets/script/asteroids.js
+++ assets/script/asteroids.js
@@ -1,7 +1,8 @@
 //code for Asteroids game
 
+/** @type {HTMLCanvasElement} */
 const FPS = 30; //game's frames per second
-const SPACESHIP_SIZE = 30; //spaceship size in px
+const SPACESHIP_SIZE = 10; //spaceship size in px
 
 let canvas = document.getElementById('asteroid-canvas');
 let ctx = canvas.getContext('2d');
@@ -19,16 +20,22 @@ setInterval(update, 1000 / FPS);
 function update() {
     //to do list to build game:
     //1. build a galaxy background
-    let background = new Image();
-    background.src = 'assets/images/nathan_anderson_unsplash_galaxy_image.jpg';
-
-    background.onload = function(){
-    ctx.drawImage(background,0, 0, canvas.width, canvas.height);   
-}
-
+    ctx.fillStyle = 'black';
+    ctx.fillRect (0, 0, canvas.width, canvas.height);   
+};
     //2. build the spaceship
-
+    ctx.strokeStyle = '#6331F6';
+    ctx.lineWidth = SPACESHIP_SIZE / 20;
+    ctx.beginPath();
+    ctx.moveTo( //the spaceship's nose
+        spaceship.x + spaceship.r * Math.cos(spaceship.a),
+        spaceship.y - spaceship.r * Math.sin(spaceship.a)
+    );
+    ctx.lineTo( //rear left of the spaceship
+        spaceship.x - spaceship.r * (Math.cos(spaceship.a) + Math.sin(spaceship.a)),
+        spaceship.y + spaceship.r * (Math.sin(spaceship.a) - Math.cos(spaceship.a))
+    );
+    ctx.stroke();
     //3. build functions to move the ship
 
     //4. build functions to rotate the ship
-}
