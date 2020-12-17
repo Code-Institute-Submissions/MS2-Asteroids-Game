130ae33ab89e62222c37b312fe79a993c5d46bd2
mickmccann <mickmccann@ymail.com>
2020-12-17 15:59:21 +0000

    Added in the spaceship properties

--- assets/script/asteroids.js
+++ assets/script/asteroids.js
@@ -1,9 +1,17 @@
 //code for Asteroids game
 
 const FPS = 30; //game's frames per second
+const SPACESHIP_SIZE = 30; //spaceship size in px
 
 let canvas = document.getElementById('asteroid-canvas');
 let ctx = canvas.getContext('2d');
+//spaceship properties
+let spaceship = {
+    x: canvas.width / 2, //spaceship appears in center of canvas
+    y: canvas.height / 2,
+    r: SPACESHIP_SIZE / 2, //size of the spaceship
+    a: 90 / 180 * Math.PI, //direction of the spaceship + convert to radians
+}
 
 //game loop to achieve the required animation
 setInterval(update, 1000 / FPS);
