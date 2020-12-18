7279468105e16247bdcc92a9ab1c28155b4ecefe
mickmccann <mickmccann@ymail.com>
2020-12-18 13:19:47 +0000

    Resolved issues with the background image displaying not at its intended resoultion. Fixed issues with the stroke not displaying properly. Now have a triangular ship!

--- assets/script/asteroids.js
+++ assets/script/asteroids.js
@@ -1,30 +1,27 @@
-//code for Asteroids game
+//Asteroids game code
 
-/** @type {HTMLCanvasElement} */
-const FPS = 30; //game's frames per second
-const SPACESHIP_SIZE = 10; //spaceship size in px
+function init() {
+//use requestAnimationFrame it is more consistent than a timer
+  window.requestAnimationFrame(draw);
+}
+const SPACESHIP_SIZE = 20; //spaceship size in px
 
+function draw() {
+  
 let canvas = document.getElementById('asteroid-canvas');
 let ctx = canvas.getContext('2d');
-//spaceship properties
-let spaceship = {
-    x: canvas.width / 2, //spaceship appears in center of canvas
-    y: canvas.height / 2,
-    r: SPACESHIP_SIZE / 2, //size of the spaceship
-    a: 90 / 180 * Math.PI, //direction of the spaceship + convert to radians
-}
 
-//game loop to achieve the required animation
-setInterval(update, 1000 / FPS);
-
-function update() {
-    //to do list to build game:
-    //1. build a galaxy background
-    ctx.fillStyle = 'black';
-    ctx.fillRect (0, 0, canvas.width, canvas.height);   
-};
+      let spaceship = {
+      x: canvas.width / 2, //spaceship appears in center of canvas
+      y: canvas.height / 2,
+      r: SPACESHIP_SIZE / 2, //size of the spaceship
+      a: 90 / 180 * Math.PI, //direction of the spaceship + convert to radians
+  } 
+      ctx.save();
+      
+ 
     //2. build the spaceship
-    ctx.strokeStyle = '#6331F6';
+    ctx.strokeStyle = 'yellow';
     ctx.lineWidth = SPACESHIP_SIZE / 20;
     ctx.beginPath();
     ctx.moveTo( //the spaceship's nose
@@ -35,7 +32,15 @@ function update() {
         spaceship.x - spaceship.r * (Math.cos(spaceship.a) + Math.sin(spaceship.a)),
         spaceship.y + spaceship.r * (Math.sin(spaceship.a) - Math.cos(spaceship.a))
     );
-    ctx.stroke();
-    //3. build functions to move the ship
 
-    //4. build functions to rotate the ship
+    ctx.lineTo( //rear right of the spaceship
+        spaceship.x - spaceship.r * (Math.cos(spaceship.a) - Math.sin(spaceship.a)),
+        spaceship.y + spaceship.r * (Math.sin(spaceship.a) + Math.cos(spaceship.a))
+    );
+    ctx.fillStyle = 'yellow';
+    ctx.closePath();
+    ctx.stroke();
+ 
+}
+
+init()
