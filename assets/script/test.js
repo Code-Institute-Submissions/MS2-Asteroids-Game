e86acf8ced99657a49a77870d4e420c699784993
mickmccann <mickmccann@ymail.com>
2021-01-06 13:40:52 +0000

    Created a let variable called render and gave it a function and moved the orange rect inside it.

--- assets/script/test.js
+++ assets/script/test.js
@@ -1,4 +1,8 @@
 let context = document.querySelector("canvas").getContext("2d");
 
-context.fillStyle = "orange";
-context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+let render = function () {
+    context.fillStyle = "orange";
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);  
+}
+
+
