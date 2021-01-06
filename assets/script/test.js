d0ee951d69c5edfedc06e1746b1be89b03cefed5
mickmccann <mickmccann@ymail.com>
2021-01-06 16:00:03 +0000

    Finished test.js to make canvas element fully responsive. Canvas width and height is responsive to all screen sizes. Now to make the game respmosive.

--- assets/script/test.js
+++ assets/script/test.js
@@ -1,8 +1,12 @@
 let context = document.querySelector("canvas").getContext("2d");
 
 let render = function () {
+    context.canvas.width = document.documentElement.clientWidth * 1.0;
+    context.canvas.height = document.documentElement.clientHeight * 1.0;
+
     context.fillStyle = "orange";
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);  
-}
+};
 
-
+window.addEventListener("resize", render);
+render();
